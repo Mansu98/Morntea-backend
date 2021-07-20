423827cd28cfeb8e6b70dc5cca5dc49642ebecb9
unknown <mansubajracharya09@gmail.com>
2021-07-20 10:04:08 +0545

    morntea deployment

--- api.js
+++ api.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv =require("dotenv");
-// const { errorHandler, notFound } = require("./middleware/errorMiddleware.js");
 
 const userRoute = require("./routes/userRoutes");
 const postRoute = require("./routes/postRoutes");
@@ -13,18 +12,15 @@ dotenv.config();
 const app = express(); // main thing
 app.use(cors());
 app.use(express.json()); // to accept json data
-// Error Handling middlewares
-// app.use(notFound);
-// app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
+
 
 app.use(express.json())
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
 
-mongoose.connect("mongodb://localhost:27017/ShareSansar",{
+mongoose.connect(process.env.CONNECTION_URL,{
     useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -37,9 +33,7 @@ mongoose.connect("mongodb://localhost:27017/ShareSansar",{
 
 
 
-app.get("/", (req,res)=>
-    res.status(200).send("Check the API in Postman or Insomiac")
-);
+const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
     console.log(`server is connected at port :${port}`)
